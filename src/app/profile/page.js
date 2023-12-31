4020794acdb7b8d4c58bf4f69f17018582fcc32a
Jacob Zhang <jaco8123@gmail.com>
2023-12-31 14:33:17 -0500

    Toast

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -3,15 +3,15 @@ import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
+import InfoBox from "@/components/layout/InfoBox";
+import SuccessBox from "@/components/layout/SuccessBox";
+import toast from "react-hot-toast";
 
 export default function ProfilePage() {
   const session = useSession();
   const [userName, setUserName] = useState("");
   const [image, setImage] = useState("");
   const { status } = session;
-  const [saved, setSaved] = useState(false);
-  const [isUploading, setIsUploading] = useState(false);
-  const [isSaving, setIsSaving] = useState(false);
 
   useEffect(() => {
     if (status === "authenticated") {
@@ -22,17 +22,21 @@ export default function ProfilePage() {
 
   async function handleProfileInfoUpdate(ev) {
     ev.preventDefault();
-    setSaved(false);
-    setIsSaving(true);
-    const response = await fetch("/api/profile", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name: userName, image }),
+    const savingPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch("/api/profile", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: userName, image }),
+      });
+      if (response.ok) resolve();
+      else reject();
     });
-    setIsSaving(false);
-    if (response.ok) {
-      setSaved(true);
-    }
+
+    await toast.promise(savingPromise, {
+      loading: "Saving...",
+      success: "Profile saved!",
+      error: "Error",
+    });
   }
 
   async function handleFileChange(ev) {
@@ -40,14 +44,24 @@ export default function ProfilePage() {
     if (files?.length === 1) {
       const data = new FormData();
       data.set("file", files[0]);
-      setIsUploading(true);
-      const response = await fetch("/api/upload", {
+
+      const uploadPromise = fetch("/api/upload", {
         method: "POST",
         body: data,
+      }).then((response) => {
+        if (response.ok) {
+          return response.json().then((link) => {
+            setImage(link);
+          });
+        }
+        throw new Error("Something went wrong");
       });
-      const link = await response.json();
-      setImage(link);
-      setIsUploading(false);
+
+      await toast.promise(uploadPromise, {
+        loading: "Uploading...",
+        success: "Upload complete",
+        error: "upload error",
+      });
     }
   }
 
@@ -63,21 +77,6 @@ export default function ProfilePage() {
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Profile</h1>
       <div className="max-w md mx-auto">
-        {saved && (
-          <h2 className="text-center bg-green-100 p-4 rounded-lg border border-green-300">
-            Profile saved!
-          </h2>
-        )}
-        {isSaving && (
-          <h2 className="text-center bg-blue-100 p-4 rounded-lg border border-blue-300">
-            Saving...
-          </h2>
-        )}
-        {isUploading && (
-          <h2 className="text-center bg-blue-100 p-4 rounded-lg border border-blue-300">
-            Uploading...
-          </h2>
-        )}
         <div className="flex gap-4 items-center">
           <div>
             <div className="p-2 rounded-lg relative max-w-[120px] ">
