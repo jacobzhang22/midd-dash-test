c06142299f9334a754305c7b44c313013f868eb2
Jacob Zhang <jaco8123@gmail.com>
2023-12-31 02:53:17 -0500

    ProfileUpdate
    3:45

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -6,6 +6,14 @@ export async function POST(req) {
   mongoose.connect(
     "mongodb+srv://midd-dash:midddash23@cluster0.usnrwxk.mongodb.net/midd-dash"
   );
+  const pass = body.password;
+  if (!pass?.length || pass.length < 5) {
+    new Error("password must be at least 5 characters");
+  }
+  const notHashedPassword = pass;
+  var salt = bcrypt.genSaltSync(10);
+  body.password = bcrypt.hashSync(notHashedPassword, salt);
+
   const createdUser = await User.create(body);
   return Response.json(createdUser);
 }
