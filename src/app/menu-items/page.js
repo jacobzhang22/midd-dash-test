ca244d2609b96d2f6033626c0cd4b7bf6fd74049
Jacob Zhang <jaco8123@gmail.com>
2024-01-02 00:08:39 -0500

    Admin panel and categories complete
    6:36:37

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -1,19 +1,11 @@
 "use client";
 import { useProfile } from "@/components/UseProfile";
-import EditableImage from "@/components/layout/EditableImage";
+import Right from "@/components/icons/Right";
 import UserTabs from "@/components/layout/UserTabs";
-import { useState } from "react";
+import Link from "next/link";
 
 export default function MenuItemsPage() {
-  const [image, setImage] = useState("");
   const { loading, data } = useProfile();
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [basePrice, setBasePrice] = useState("");
-
-  async function handleFormSubmit(ev) {
-    ev.preventDefault();
-  }
 
   if (loading) {
     return "Loading user info...";
@@ -24,39 +16,14 @@ export default function MenuItemsPage() {
   }
 
   return (
-    <section className="mt-8">
+    <section className="mt-8 max-w-md mx-auto">
       <UserTabs isAdmin={true} />
-      <form onSubmit={handleFormSubmit} className="mt-8 max-w-md mx-auto">
-        <div
-          className="grid items-start gap-4"
-          style={{ gridTemplateColumns: ".3fr .7fr" }}
-        >
-          <div>
-            <EditableImage link={image} setLink={setImage} />
-          </div>
-          <div className="grow">
-            <label>Item name</label>
-            <input
-              type="text"
-              value={name}
-              onChange={(ev) => setName(ev.target.value)}
-            />
-            <label>Description</label>
-            <input
-              type="text"
-              value={name}
-              onChange={(ev) => setName(ev.target.value)}
-            />
-            <label>Base price</label>
-            <input
-              type="text"
-              value={name}
-              onChange={(ev) => setName(ev.target.value)}
-            />
-            <button type="submit">Save</button>
-          </div>
-        </div>
-      </form>
+      <div className="mt-8">
+        <Link className="button flex" href={"/menu-items/new"}>
+          <span>Create new menu item</span>
+          <Right />
+        </Link>
+      </div>
     </section>
   );
 }
