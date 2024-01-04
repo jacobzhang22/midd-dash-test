36a9d46c1bb89ff041d93566c6a60f58a60da4ba
Jacob Zhang <jaco8123@gmail.com>
2024-01-03 21:38:10 -0500

    smallupdatestripe
    15:13

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -21,15 +21,28 @@ export default function CartPage() {
   }, [profileData]);
 
   //calculate item cost
-  let total = 0;
+  let subtotal = 0;
   for (const p of cartProducts) {
-    total += cartProductPrice(p);
+    subtotal += cartProductPrice(p);
   }
 
   function handleAddressChange(propName, value) {
     setAddress((prevAddress) => {
       ({ ...prevAddress, [propName]: value });
     });
+  }
+
+  async function proceedToCheckout(ev) {
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        address,
+        cartProducts,
+      }),
+    });
+    const link = await response.json();
+    window.location = link;
   }
 
   return (
@@ -70,19 +83,27 @@ export default function CartPage() {
                 </div>
               </div>
             ))}
-          <div className="py-2 text-right pr-16">
-            <span className="text-gray-500">Subtotal:</span>
-            <span className="text-lg font-semibold pl-2">${total}</span>
+          <div className="py-2 pr-16 flex justify-end items-center">
+            <div className="text-gray-500">
+              Subtotal:
+              <br /> Delivery:
+              <br /> Total:
+            </div>
+            <div className="text-lg font-semibold pl-2 text-right">
+              ${subtotal}
+              <br /> $5.00
+              <br />${subtotal + 5}
+            </div>
           </div>
         </div>
         <div className="bg-gray-100 p-4 rounded-lg">
           <h2>Checkout</h2>
-          <form>
+          <form onSubmit={proceedToCheckout}>
             <AddressInputs
               addressProps={address}
               setAddressProp={handleAddressChange}
             />
-            <button type="submit">Pay ${total}</button>
+            <button type="submit">Pay ${subtotal + 5}</button>
           </form>
         </div>
       </div>
